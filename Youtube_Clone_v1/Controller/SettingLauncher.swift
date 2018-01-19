ba522bfea9a994d7f3faac973281417f7a632b78
kitiwat chanluthin <aurelia@kitiwats-MacBook-Pro.local>
2018-01-19 15:35:59 +0700

    done ep8

---
+++ Youtube_Clone_v1/Controller/SettingLauncher.swift
@@ -0,0 +1,107 @@
+//
+//  SettingLauncher.swift
+//  Youtube_Clone_v1
+//
+//  Created by kitiwat chanluthin on 19/1/18.
+//  Copyright © 2018 kitiwat chanluthin. All rights reserved.
+//
+
+import UIKit
+
+
+class Setting:NSObject{
+    let name:String
+    let imageName:String
+    
+    init(name:String,imageName:String) {
+        self.name = name
+        self.imageName = imageName
+    }
+}
+
+
+class SettingLauncher: NSObject,UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+    
+    let blackView = UIView()
+    
+    let collectionView:UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.white
+        return cv
+    }()
+    
+    let cellId  = "cellId"
+    let cellHeight:CGFloat = 50
+    let settings:[Setting] = {
+       return [Setting(name: "Settings", imageName: "setting2"),Setting(name: "Terms & privacy policy", imageName: "privacy"),Setting(name: "Send Feedback", imageName: "feedback"),Setting(name: "Help", imageName: "help"),Setting(name: "Switch Account", imageName: "switch_account"),Setting(name: "Cancel", imageName: "cancel")]
+    }()
+    
+    func showSettings(){
+        
+        if let window = UIApplication.shared.keyWindow{
+            
+            blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+            blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
+            
+            window.addSubview(blackView)
+            window.addSubview(collectionView)
+            
+            let height:CGFloat = CGFloat(settings.count ) * cellHeight
+            let y = window.frame.height - height
+            collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
+            
+            blackView.frame = window.frame
+            blackView.alpha = 0
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.blackView.alpha = 1
+                self.collectionView.frame = CGRect(x: 0, y: y, width: self.collectionView.frame.width , height: self.collectionView.frame.height)
+            }, completion: nil)
+            
+            
+        }
+    }
+    
+    @objc func handleDismiss(){
+        UIView.animate(withDuration: 0.35) {
+            self.blackView.alpha = 0
+            if let window = UIApplication.shared.keyWindow{
+                self.collectionView.frame = CGRect(x: 0, y: window.frame.height, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            }
+        }
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return settings.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SettingCell
+       
+        let setting = settings[indexPath.item]
+        cell.setting = setting
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0 
+    }
+    
+    
+    override init() {
+        super.init()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(SettingCell.self, forCellWithReuseIdentifier: cellId)
+        
+    }
+}
---
+++ Youtube_Clone_v1/View/SettingCell.swift
@@ -0,0 +1,59 @@
+//
+//  SettingCell.swift
+//  Youtube_Clone_v1
+//
+//  Created by kitiwat chanluthin on 19/1/18.
+//  Copyright © 2018 kitiwat chanluthin. All rights reserved.
+//
+
+import UIKit
+
+class SettingCell: BaseCell {
+    
+    override var isSelected: Bool{
+        didSet{
+            backgroundColor = isSelected ? UIColor.darkGray : UIColor.white
+            nameLabel.textColor = isSelected ? UIColor.white : UIColor.black
+            iconImageView.tintColor = isSelected ? UIColor.white : UIColor.darkGray
+        }
+    }
+    
+    var setting:Setting?{
+        didSet{
+            nameLabel.text = setting?.name
+            iconImageView.image = UIImage(named:(setting?.imageName)!)?.withRenderingMode(.alwaysTemplate)
+            iconImageView.tintColor = UIColor.darkGray
+        }
+    }
+    
+    let nameLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Setting"
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    let iconImageView:UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(named:"setting")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+       
+        addSubview(nameLabel)
+        addSubview(iconImageView)
+        
+        addConstraintsWithFormat(format: "H:|-8-[v0(30)]-8-[v1]|", views: iconImageView,nameLabel)
+        
+        
+        addConstraintsWithFormat(format: "V:|[v0]|", views: nameLabel)
+        
+        addConstraintsWithFormat(format: "V:[v0(30)]", views: iconImageView)
+        
+        addConstraint(NSLayoutConstraint(item: iconImageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+        
+    }
+}
